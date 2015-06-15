939b03f2db7d463ed345a4c902ac2e362de35bdf
chenlei <1061867821@qq.com>
2015-06-15 10:46:01 +0800

    添加忘记密码activity  ResetPassActivity1、RestPassActivity2 及对应布局文件activity_resetpass1、activity_resetpass2 Signed-off-by: chenlei <1061867821@qq.com>

---
+++ src/com/tongjo/girlswish/ui/ResetPassActivity2.java
@@ -0,0 +1,13 @@
+package com.tongjo.girlswish.ui;
+
+import com.tongjo.girlswish.R;
+
+import android.os.Bundle;
+
+public class ResetPassActivity2 extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_resetpass2);
+	}
+}
